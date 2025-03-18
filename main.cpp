ac46f86ad0f606e81a795b80e7eeaf0d68501e57
fbtom <92322227+fbtom@users.noreply.github.com>
2025-03-19 00:03:00 +0100

    Feature #000003: Use default camera and display in window (#4)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include "opencv2/opencv.hpp"
 
-using std::cout;
+using std::cerr;
 
 int main() 
 {
-  cout << "Hello Dear World! I am Campo!\n";  
+  cv::VideoCapture cam{0};
+  if(!cam.isOpened())
+  {
+    cerr << "Cannot open Your camera\n";
+  }
+  cv::Mat frame{};
+  while(cv::waitKey(1) != 27)
+  {
+    cam >> frame; 
+    if(frame.empty()) 
+    {
+      break;
+    }
+
+    cv::imshow("Preview", frame);
+  }
   return 0;
 }
